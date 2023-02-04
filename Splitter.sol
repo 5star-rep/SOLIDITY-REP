93899a34117aab9917778a07473b6a25349d00aa
5star-rep <111691547+5star-rep@users.noreply.github.com>
2023-02-04 19:08:54 +0100

    Update Splitter.sol

--- Splitter.sol
+++ Splitter.sol
@@ -8,6 +8,7 @@ contract Splitter {
 
     address private owner;
     address payable [] private recipients;
+    uint256 private Passcode;
     event TransferReceived(address _from, uint _amount);
 
     modifier isOwner() {
@@ -15,17 +16,20 @@ contract Splitter {
         _;
     }
 
-    constructor() payable {
+    constructor(uint256 passcode) payable {
         owner = msg.sender;
+        Passcode = passcode;
     }
 
-    function SetRecipients(address payable [] memory _addrs) public isOwner {
+    function SetRecipients(address payable [] memory _addrs, uint256 passcode) public isOwner {
+        require(passcode == Passcode, "Wrong passcode");
         for(uint i=0; i < _addrs.length; i++) {
             recipients.push(_addrs[i]);
         }
     }
 
-    function ChangeOwner(address newOwner) public isOwner {
+    function ChangeOwner(address newOwner, uint256 passcode) public isOwner {
+        require(passcode == Passcode, "Wrong passcode");
         owner = newOwner;
     }
 
