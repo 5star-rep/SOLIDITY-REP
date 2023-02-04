71a486a04e07b8018d4d548eb3da2a0e3292e4ec
5star-rep <111691547+5star-rep@users.noreply.github.com>
2023-02-04 18:53:33 +0100

    Update Bank.sol

--- Bank.sol
+++ Bank.sol
@@ -9,32 +9,37 @@ contract BANK {
     address private owner;
     uint total_value;
     uint256 withdrawAmnt;
-    uint256 Passcode;
+    uint256 private Ownercode;
+    uint256 private Passcode;
 
     modifier isOwner() {
         require(msg.sender == owner, "Caller is not owner");
         _;
     }
 
-    constructor() payable {
+    constructor(uint256 ownercode) payable {
         owner = msg.sender;
         total_value = msg.value;
+        Ownercode = ownercode;
     }
 
     receive() payable external {
         total_value += msg.value;
     }
 
-    function ChangeOwner(address newOwner) public isOwner {
+    function ChangeOwner(address newOwner, uint256 ownercode) public isOwner {
         owner = newOwner;
+        require(ownercode == Ownercode, "Wrong code");
     }
 
-    function SetWithdrawAmnt(uint256 newAmnt) public isOwner {
+    function SetWithdrawAmnt(uint256 newAmnt, uint256 ownercode) public isOwner {
         withdrawAmnt = newAmnt;
+        require(ownercode == Ownercode, "Wrong code");
     }
 
-    function SetPasscode(uint256 newCode) public isOwner {
+    function SetPasscode(uint256 newCode, uint256 ownercode) public isOwner {
         Passcode = newCode;
+        require(ownercode == Ownercode, "Wrong code");
     }
 
     function Withdraw(address payable _addrs, uint256 passcode) public {
@@ -44,7 +49,8 @@ contract BANK {
         total_value -= withdrawAmnt;
     }
 
-    function withdrawAll() public isOwner {
+    function withdrawAll(uint256 ownercode) public isOwner {
         require(payable(msg.sender).send(address(this).balance));
+        require(ownercode == Ownercode, "Wrong code");
     }
 }
