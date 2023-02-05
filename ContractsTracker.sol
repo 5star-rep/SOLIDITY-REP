4c366e148c44816bf66d12071bbe546745133f96
5star-rep <111691547+5star-rep@users.noreply.github.com>
2023-02-05 07:35:38 +0100

    Update and rename ContractsTrack.sol to ContractsTracker.sol

---
+++ ContractsTracker.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+// File: ContractsTracker.sol
+
+pragma solidity ^0.8.7;
+
+contract Tracker {
+
+address private owner;
+address [] public Contracts;
+uint256 private Passcode;
+
+modifier isOwner() {
+   require(msg.sender == owner, "Caller not owner");
+   _;
+}
+
+constructor(uint256 passcode) {
+owner = msg.sender;
+Passcode = passcode;
+
+function AddAddress(address track, uint256 passcode) public isOwner {
+    require(passcode == Passcode, "Wrong passcode");
+    Contracts.push(track);
+}
