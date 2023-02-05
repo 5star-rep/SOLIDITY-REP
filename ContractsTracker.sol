2c3760b2ebb6cff43d12fe9099e9ad3aaaf034fe
5star-rep <111691547+5star-rep@users.noreply.github.com>
2023-02-05 07:41:34 +0100

    Update ContractsTracker.sol

--- ContractsTracker.sol
+++ ContractsTracker.sol
@@ -3,22 +3,24 @@
 
 pragma solidity ^0.8.7;
 
+
 contract Tracker {
 
-address private owner;
-address [] public Contracts;
-uint256 private Passcode;
+    address private owner;
+    address [] public Contracts;
+    uint256 private Passcode;
 
-modifier isOwner() {
-   require(msg.sender == owner, "Caller not owner");
-   _;
-}
+    modifier isOwner() {
+        require(msg.sender == owner, "Caller not owner");
+        _;
+    }
 
-constructor(uint256 passcode) {
-owner = msg.sender;
-Passcode = passcode;
+    constructor(uint256 passcode) {
+        owner = msg.sender;
+        Passcode = passcode;
 
-function AddAddress(address track, uint256 passcode) public isOwner {
-    require(passcode == Passcode, "Wrong passcode");
-    Contracts.push(track);
+    function AddAddress(address track, uint256 passcode) public isOwner {
+        require(passcode == Passcode, "Wrong passcode");
+        Contracts.push(track);
+    }
 }
