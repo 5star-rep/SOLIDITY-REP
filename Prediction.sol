c42846f146e55704716ba9544f388ff092d89b80
5star-rep <111691547+5star-rep@users.noreply.github.com>
2023-05-21 15:33:31 +0100

    Update Prediction.sol

--- Prediction.sol
+++ Prediction.sol
@@ -1,2 +1,17 @@
-pragma solidity v0:8:0;
-// SPDL Identify License MIT;
+pragma solidity ^0.8.7;
+// SPDX-Identify-License: MIT
+
+contract PREDICTION {
+
+   address public owner;
+   address payable private feeTreasury;
+   uint256 public total_value;
+   uint256 public cost = 1 ether;
+   uint256 public jackpot = 5 ether;
+   uint256 public fee = 1 ether;
+   uint256 private roller;
+   string public win;
+   
+   constructor() payable {
+       owner = msg.sender;
+   }
